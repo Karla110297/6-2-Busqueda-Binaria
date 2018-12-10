b31a45130b83417d6e3da7b76941e60bbb79b52e
Karla110297 <dinorakarla@hotmail.com>
2018-12-10 12:02:46 -0800

    Add project files.

---
+++ 6-2 Busqueda Binaria/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_2_Busqueda_Binaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
